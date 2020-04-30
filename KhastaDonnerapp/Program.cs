bab2b9ef15803dde8d3d158fe8c6bb6324c4c87e
Baharan <fessuq344@gmail.com>
2020-04-30 10:48:12 +0200

    Write Khasta 2020

--- KhastaDonnerapp/Program.cs
+++ KhastaDonnerapp/Program.cs
@@ -6,7 +6,7 @@ namespace KhastaDonnerapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Khasta!");
         }
     }
 }
